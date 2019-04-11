654e7e4f982f564f67e57766fd6e6bf1aadd3002
Conor Tolan <ctolan@gmail.com>
2019-04-11 08:39:54 +0100

    Added boto call of lambda

--- setup_cm.py
+++ setup_cm.py
@@ -4,13 +4,12 @@ client = boto3.client('autoscaling')
 
 response = client.update_auto_scaling_group(
     AutoScalingGroupName='CM_ASG',
-    MinSize=0,
-    MaxSize=0
+    MinSize=3,
+    MaxSize=3
 )
-"""
+
 response = client.set_desired_capacity(
     AutoScalingGroupName='CM_ASG',
     DesiredCapacity=3,
     HonorCooldown=False
 )
-"""
